ed78dcdeef8dc3cc4e0782b8016ffe9be4c21d9b
naomi-porter <73915562+naomi-porter@users.noreply.github.com>
2021-01-26 10:55:44 +1300

    working on states

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
 
-export const Pixel = () => (
-    <div style={{
+import React, { useState } from 'react'
+
+const Pixel = () => {
+    // const styleObject=
+    const [style, setStyle] = useState ({
         height: '100px',
         width: '100px',
         backgroundColor: 'deeppink'
+    })
 
-    }}></div>
-)
+    setStyle({
+        height: '100px',
+        width: '100px',
+        backgroundColor: 'blue'
+    })
+     
+
+    return(
+         <div style= {style}>
+         </div>
+         )
+}
+
+
+
 
 export default Pixel
