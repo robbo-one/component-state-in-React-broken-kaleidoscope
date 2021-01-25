dc215be1dcfaf27cb1cab69edc12ffe452c94472
robbo-one <73087376+robbo-one@users.noreply.github.com>
2021-01-26 10:26:47 +1300

    TheGoldenRod

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Poixel from './WhateverYouLike.jsx'
 
 const App = () => {
   return (
+    <>
     <div>React development has begun!</div>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    </>
   )
 }
 
---
+++ client/components/WhateverYouLike.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Poixel = () => {
+  return (
+    <div style={{
+      height:'100px',
+      width:'500px',
+      backgroundColor:'DarkGoldenRod'
+    }}>
+    </div>
+  )
+}
+
+export default Poixel
