a635ca1c9a26794ff70b8f2bae6627b34c3f2889
Jack Bennett <73858124+j-ack-bennett@users.noreply.github.com>
2021-01-26 10:55:58 +1300

    Reeled in a blue fin

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,112 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Poixel from './WhateverYouLike.jsx'
 
 const App = () => {
+  
   return (
     <>
-    <div>React development has begun!</div>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
+    <Poixel/>
     <Poixel/>
     <Poixel/>
     <Poixel/>
--- client/components/WhateverYouLike.jsx
+++ client/components/WhateverYouLike.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Poixel = () => {
+
+  const [style, setStyle] = useState({
+    height: '100px',
+    width: '100px',
+    backgroundColor: 'DarkGoldenRod'
+  })
+
   return (
-    <div style={{
-      height:'100px',
-      width:'500px',
-      backgroundColor:'DarkGoldenRod'
-    }}>
-    </div>
+    <div onClick={() => {
+      setStyle({
+        height: '100px',
+        width: '100px',
+        backgroundColor: randomHexColor()
+      })
+    }} style={ style }></div>
   )
 }
+
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
 export default Poixel
