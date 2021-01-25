306a32470b1a16c7d10efa155d5d00daa5d56309
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2021-01-26 10:50:06 +1300

    onclick pixels working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,32 +1,33 @@
 import React from 'react'
 import Pixel from './Pixel'
 
+
+
 const App = () => {
+  
+  
+  // console.log(clickHandler)
+  const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
   return (
+    
     <>
-    <div>React development has begun!</div>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
-    <Pixel/>
+    <Pixel />
+    <Pixel />
+    <Pixel />
+    <Pixel />
+    <Pixel />
+    <Pixel />
+    <Pixel />
+    <Pixel />
+    <Pixel />
+    <Pixel />
+    <Pixel />
+    <Pixel />
+    <Pixel />
+    <Pixel />
+    <Pixel />
+   
     </>
   )
 }
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,12 +1,21 @@
 import React, {useState} from 'react'
 
-const Pixel = () => {
 
-  
-  const [style] = useState({height:100, width: 100, backgroundColor: 'cornflowerblue'})
 
+const randomHexColor = () =>
+  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+const Pixel = (props) => {
+  const [style, setStyle] = useState({height:100, width: 100, backgroundColor: randomHexColor()})
+  const clickHandler = evt => {
+    setStyle({
+      backgroundColor: randomHexColor(),
+      height:100, 
+      width: 100
+    })
+  }
+  console.log(style)
   return (
-  <div style={style}>
+  <div onClick={clickHandler} style={style}>
   </div>
   )
 }
