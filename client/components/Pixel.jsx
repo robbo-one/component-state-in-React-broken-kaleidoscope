0bd1c069b57349f6bde46171125d9cd8ca031a7d
Rosemary-Hawkes <15070793+Rosemary-Hawkes@users.noreply.github.com>
2021-01-26 10:46:56 +1300

    Add click handler to set style

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -4,11 +4,17 @@ import { useState } from 'react'
 const randomHexColor = () =>
 `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
+
 const Pixel = () => {
     const [style, setStyle] = useState({ height: "50px", width: '50px', backgroundColor: randomHexColor() })
-    
+
+    const clickHandler = evt => {
+        setStyle(
+            { height: "50px", width: '50px', backgroundColor: randomHexColor() }
+        )
+      }
     return (
-        <div style={style}></div>
+        <div onClick={() => clickHandler()}style={style}></div>
     )
 }
 
