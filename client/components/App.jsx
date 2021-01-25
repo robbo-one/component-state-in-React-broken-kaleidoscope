f511d6d7c0d4b2843e4ca17ce74399349a587c33
Lukas <63496113+lukas-jj@users.noreply.github.com>
2021-01-26 11:41:08 +1300

    useEffect works

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,9 +3,6 @@ import Pixel from './Pixel'
 
 const App = () => {
 
-  
-
-
   return Array(1000).fill(0).map(() => { return <Pixel/>})
 }
 
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
 
 const randomHexColor = () =>
 `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
-
 
 const Pixel = () => {
     const [style, setStyle] = useState({ height: "50px", width: '50px', backgroundColor: randomHexColor() })
@@ -14,6 +13,8 @@ const Pixel = () => {
         )
       }
 
+
+      
       const turnGreen = evt => {
         setStyle(
             { height: "50px", width: '50px', backgroundColor: 'green' }
@@ -27,8 +28,19 @@ const Pixel = () => {
         )
       }
 
+//copy and pasted from solutions
 
 
+      useEffect(() => {
+        const interval = setInterval(() => {
+          setStyle( 
+            { height: "50px", width: '50px', backgroundColor: randomHexColor() }
+          )
+        }, 10)
+        return () => clearInterval(interval)
+      }, [])
+
+// 
     return (
         <div onContextMenu={turnBlack} onMouseEnter={() => turnGreen()} onClick={() => clickHandler()}style={style}></div>
     )
