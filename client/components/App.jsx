5787884503c4a9fc98963a7f347a1828a05d460a
Tom May <73091215+tom-may@users.noreply.github.com>
2021-01-26 10:17:59 +1300

    add pixel component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import Pixel from './Pixel.jsx'
+
+
 const App = () => {
   return (
+    <>
     <div>React development has begun!</div>
+    <Pixel />
+    </>
   )
 }
+
+
 
 export default App
---
+++ client/components/Pixel.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Pixel = () => (
+    <div></div>
+)
+
+export default Pixel
