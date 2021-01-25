3cb2637307e38a9f1c37782c5bb4a6b2d19c30dd
Rosemary-Hawkes <15070793+Rosemary-Hawkes@users.noreply.github.com>
2021-01-26 11:26:23 +1300

    Add array of pixels

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,22 +2,11 @@ import React from 'react'
 import Pixel from './Pixel'
 
 const App = () => {
-  return (
-    <div id="App" ><div>React development has begun!</div>
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-      <Pixel />
-    </div>
-  )
+
+  
+
+
+  return Array(1000).fill(0).map(() => { return <Pixel/>})
 }
 
 export default App
