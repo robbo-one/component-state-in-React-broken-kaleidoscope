799e15ee14a208f529a9bfb367c2b8ebfc287af1
Michael Parkin <73506758+mike-parkin@users.noreply.github.com>
2021-01-26 10:57:27 +1300

    color changes on hover

--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
 
 const randomHexColor = () =>
-  `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
+  `#${Math.floor(Math.random() * 0x1000000)
+    .toString(16).padStart(6, 0)}`
 
 const Pixel = () => { 
+  const clickHandler = evt => {
+    setStyle({
+      height: "50px",
+      width: "50px",
+      backgroundColor: randomHexColor()
+    })
+  }
   const [style, setStyle] = useState({
     height: "50px",
     width: "50px",
     backgroundColor: randomHexColor()
   })
-  return(<div style={style}>
+  return(<div style={style} onMouseEnter={() => clickHandler()}>
   </div>)
 }
 
