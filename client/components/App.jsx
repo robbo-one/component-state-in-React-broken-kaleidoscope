06c072958e14bc638fa2a349bd05ab1ad4f7eb85
Dylan Hodges <73857570+dylan-hodges313@users.noreply.github.com>
2021-01-26 16:45:51 +1300

    circles

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import Pixel from './Pixel'
 
 const pixels = []
-for(let i=0; i< 10000; i++) {
+for(let i=0; i < 500; i++) {
   pixels[i] = <Pixel />
+  console.log(Pixel)
 }
 
 const App = () => {
   return (
     <div className ="container">
-      {pixels}
+      {pixels}      
     </div>
   )
 }
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 const randomHexColor = () =>
   `#${Math.floor(Math.random() * 0x1000000)
@@ -7,16 +7,16 @@ const randomHexColor = () =>
 const Pixel = () => { 
   const hoverHandler = evt => {
     setStyle({
-      height: "6px",
-      width: "6px",
+      height: "20px",
+      width: "20px",
       backgroundColor: randomHexColor()
     })
   }
 
   const onDoubleClick = (evt) => {
     setStyle({
-      height: "6px",
-      width: "6px",
+      height: "20px",
+      width: "20px",
       backgroundColor: 'white'
     })
   }
@@ -24,8 +24,8 @@ const Pixel = () => {
   const onDragEnter = (evt) => {
     evt.preventDefault() 
     setStyle({
-      height: "6px",
-      width: "6px",   
+      height: "20px",
+      width: "20px",   
       backgroundColor: 'yellow' 
     })
   } 
@@ -33,24 +33,49 @@ const Pixel = () => {
   const contextMenu = (evt) => {
     evt.preventDefault() 
     setStyle({
-      height: "6px",
-      width: "6px",   
+      height: "20px",
+      width: "20px",   
       backgroundColor: 'black' 
     })
   } 
 
   const [style, setStyle] = useState({
-    height: "6px",
-    width: "6px",
+    height: "20px",
+    width: "20px",
     backgroundColor: randomHexColor()
   })
 
-  return(<div style={style} 
+  const startInterval = (evt) => {
+    setInterval(() => {
+      setStyle({
+        height: "20px",
+        width: "20px",
+        backgroundColor: randomHexColor()
+      })
+    }, 2000)
+  }
+
+  useEffect(() => {
+    const randoNum = Math.floor(Math.random() * (1 + 75 - 2)) + 2 + 'px'
+    const randoNum2 = Math.floor(Math.random() * (1 + 75 - 2)) + 2 + 'px'
+    setInterval(() => {
+      setStyle({
+        height: randoNum,
+        width: randoNum2,
+        backgroundColor: randomHexColor()
+      })
+    }, 1500)
+  })
+
+
+
+  return(<div className='pixel' style={style} 
   onMouseEnter={() => hoverHandler()}
   // onMouseLeave={() => hoverHandler()}
   onContextMenu={(evt) => contextMenu(evt)}
   onDoubleClick={(evt) => onDoubleClick(evt)}
-  onDragEnter={(evt) => onDragEnter(evt)}>
+  onDragEnter={(evt) => onDragEnter(evt)}
+  onClick={(evt) => startInterval(evt)}>
 
   </div>)
 }
