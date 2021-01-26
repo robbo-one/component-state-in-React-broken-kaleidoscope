f971e69ef5903541394db8c17394403a50d73f08
logan-webber <73917857+logan-webber@users.noreply.github.com>
2021-01-26 15:58:43 +1300

    seizure mode activated

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,8 @@ import Pixel from './Pixel'
 
 
 const App = () => {
-  return Array(10000).fill(0).map(() => <Pixel />)
+  return Array(750).fill(0).map((arr, i) => <Pixel key={i} />
+  )
 }
 
 export default App
--- client/components/Pixel.jsx
+++ client/components/Pixel.jsx
@@ -3,19 +3,28 @@ import React, { useState } from 'react'
  const randomHexColor = () =>
   `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, 0)}`
 
+  
 
-const Pixel = (props) => { 
-  const [currentColour, setNewColour] = useState(randomHexColor())
+  
+  
+  const Pixel = (props) => { 
+    const [currentColour, setNewColour] = useState(randomHexColor())
+    function timer() {
+      setInterval(() => {
+        console.log('cheese')
+      setNewColour(randomHexColor())
+      
+      }, 500);
+    }
   return(
   <>
-  <div onClick = {() => setNewColour(randomHexColor)} 
-    onMouseEnter = {() => setNewColour('red')}
-    onDoubleClick ={() => setNewColor ('skylue')}
-  
+  <div onClick = {() => timer()} 
+    onMouseEnter = {() => timer()}
+    onDoubleClick ={() => timer()}
     style={{
     fontFamily: 'Times New Roman',
-    height: 15,
-    width: 15,
+    height: 50,
+    width: 50,
     backgroundColor: currentColour
   }}></div>
   </>
